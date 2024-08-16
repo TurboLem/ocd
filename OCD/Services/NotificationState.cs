893d9f63f66498995d4c03b400e3cde6970e8351
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-08-16 09:10:44 +0200

    added notification item for comments

---
+++ OCD/Services/NotificationState.cs
@@ -0,0 +1,20 @@
+﻿namespace OCD.Services
+{
+    public class NotificationState
+    {
+        public event Action OnChange;
+        private int _newCommentsCount;
+
+        public int NewCommentsCount
+        {
+            get => _newCommentsCount;
+            set
+            {
+                _newCommentsCount = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
